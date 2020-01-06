b3ba0fc648373a604b5f375cd9fb180ad54958be
par1si <pparisi13@gmail.com>
2020-01-05 18:49:25 -0800

    pushing stable version before I install the Optimizely FS SDK's

---
+++ public/js/journals.js
@@ -0,0 +1,15 @@
+const textarea = document.getElementById('textarea');
+const button = document.getElementById('button')
+
+function expandViewPort() {
+    textarea.style.width = '85%'
+    textarea.style.height = '15rem';
+}
+
+function collapseViewPort() {
+    textarea.style.width = '40%'
+    textarea.style.height = '3rem';
+}
+
+textarea.addEventListener("click", expandViewPort);
+textarea.addEventListener("blur", collapseViewPort);
